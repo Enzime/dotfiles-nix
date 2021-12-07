696f63bc1da92d9259a816f9dcf766fce394d035
Michael Hoang <enzime@users.noreply.github.com>
2021-12-08 09:34:44 +1100

    well that was easier than I thought

--- overlays/nixpkgs/flake.nix
+++ overlays/nixpkgs/flake.nix
@@ -5,6 +5,7 @@
     overlay = final: prev: {
       mpv = unstable.legacyPackages.x86_64-linux.mpv;
       neovim = unstable.legacyPackages.x86_64-linux.neovim;
+      nix-direnv = unstable.legacyPackages.x86_64-linux.nix-direnv;
     };
   };
 }
