cbd091213bf130c07ed21500c1a01256a819316b
Michael Hoang <enzime@users.noreply.github.com>
2026-02-26 21:17:25 +0100

    utm: 5.0.1 -> 5.0.2

--- overlays/utm.nix
+++ overlays/utm.nix
@@ -1,9 +1,9 @@
 self: super: {
   utm = super.utm.overrideAttrs (old: {
-    version = "5.0.1";
+    version = "5.0.2";
 
     src = old.src.overrideAttrs {
-      hash = "sha256-fP8TVTrZLxy0+YQb6skZM7u6kBmb9BMmbjy49A0fdlg=";
+      hash = "sha256-5PYeanoxf+hdhVcVooRSZOrTdR5WzagIr43I4BVu0bk=";
     };
   });
 }
