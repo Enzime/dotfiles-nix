712d93b1676047bc279b1e51c29fda6ec6a6759e
Michael Hoang <enzime@users.noreply.github.com>
2026-04-16 11:00:36 +0200

    gaia: disable remote builds while using Lix

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   ...
 }:
@@ -19,28 +18,6 @@
 
   zramSwap.enable = true;
   zramSwap.memoryPercent = 250;
-
-  nix.distributedBuilds = true;
-
-  # Use ssh-ng for trustless remote building of input-addressed derivations
-  # i.e. not requiring remote user to be a trusted-user
-  nix.buildMachines = [
-    {
-      protocol = "ssh-ng";
-      hostName = "clan.lol";
-      sshUser = "builder";
-      sshKey = config.clan.core.vars.generators.nix-remote-build.files.key.path;
-      system = "x86_64-linux";
-      supportedFeatures = [
-        "kvm"
-        "benchmark"
-        "big-parallel"
-        "nixos-test"
-        "uid-range"
-      ];
-      maxJobs = 96;
-    }
-  ];
 
   # Check that this can be bumped before changing it
   system.stateVersion = "25.11";
