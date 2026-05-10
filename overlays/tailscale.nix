56d82f232a570578cd57e7b42d0657f64a40b053
Michael Hoang <enzime@users.noreply.github.com>
2026-05-11 00:09:26 +0200

    tailscale: fix bugs in exit node implementation

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -3,8 +3,8 @@ self: super: {
     patches = (old.patches or [ ]) ++ [
       (super.fetchpatch {
         name = "support-exit-nodes-on-macos.patch";
-        url = "https://github.com/Enzime/tailscale/commit/2de687882bd4480ad538fb613ead1fb5339b6a00.patch";
-        hash = "sha256-xTQ4pbcj0AhTYfJMQSho6Mxf42bOCQuymiFKm+8Y07A=";
+        url = "https://github.com/Enzime/tailscale/commit/f417eda370d18792894d336cf1ee576a372cfc85.patch";
+        hash = "sha256-JusxAIQ72Uqn6OMcA6xFn8OEqNLrmGZxoDQU6CBPLVs=";
       })
     ];
   });
